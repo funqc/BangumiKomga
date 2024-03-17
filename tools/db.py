ac0153faa390d53a0d15a4f6f820d2eb61ca4db4
funqc <fu92811@163.com>
2024-03-17 11:10:53 +0800

    change db path

--- tools/db.py
+++ tools/db.py
@@ -32,7 +32,7 @@ def upsert_book_record(conn, book_id, subject_id, update_success, book_name):
 
 def initSqlite3():
     # Create a connection to the sqlite database
-    conn = sqlite3.connect("recordsRefreshed.db")
+    conn = sqlite3.connect("config/recordsRefreshed.db")
     cursor = conn.cursor()
     cursor.execute(
         '''CREATE TABLE IF NOT EXISTS refreshed_series (series_id text primary key,subject_id text ,update_success BOOLEAN,series_name text,bangumi_name text,refresh_time text )''')
